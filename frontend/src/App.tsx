e978000ecf54398ded4890d563e58454e4215afe
Janith Chathuranga <janithsamarasinghe1999@gmail.com>
2026-06-20 18:07:55 +0530

    chore: revert App to use old login page and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import AdminLayout from "layouts/admin";
-import AuthLayout from "layouts/auth";
-import PharmaLayout from "layouts/pharma";
-import ProtectedRoute from "components/auth/ProtectedRoute";
-import { Toaster } from "components/ui/sonner";
-import StartupPage from "pages/StartupPage";
+import React from "react";
+import { AuthProvider } from "./auth/AuthContext";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <>
-    <Routes>
-      {/* Startup / placeholder page */}
-      <Route path="startup" element={<StartupPage />} />
-
-      {/* Public auth routes */}
-      <Route path="auth/*" element={<AuthLayout />} />
-
-      {/* Protected admin routes */}
-      <Route
-        path="admin/*"
-        element={
-          <ProtectedRoute>
-            <AdminLayout />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* Pharma public routes */}
-      <Route path="pharma/*" element={<PharmaLayout />} />
-
-      {/* Default redirect */}
-      <Route path="/" element={<Navigate to="/admin/home" replace />} />
-    </Routes>
-    <Toaster position="top-right" richColors />
-    </>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   );
 };
 
